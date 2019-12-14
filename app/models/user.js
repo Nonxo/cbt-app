4002de91bb9a1be117d7c6e7a4228f0bc0bb7ee8
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-14 13:36:22 +0100

    Modified user model , adding hashing of password

--- app/models/user.js
+++ app/models/user.js
@@ -2,18 +2,22 @@
  * Module dependencies
  */
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const uuid4 = require("uuid/v4");
 
 /**
  * User schema
  */
 
 const UserSchema = new Schema({
-  name: { type: String, default: '' },
-  email: { type: String, default: '' },
-  hashed_password: { type: String, default: '' },
-  salt: { type: String, default: '' }
+  id: { type: String, default: uuid4 },
+  name: { type: String, default: "" },
+  email: { type: String, default: "" },
+  hashed_password: { type: String, default: "" },
+  salt: { type: String, default: "" }
 });
 
 /**
@@ -26,7 +30,50 @@ const UserSchema = new Schema({
 /**
  * Methods
  */
+UserSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(Number(12)).toString("hex");
+  return (this.password = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+    .toString("hex"));
+};
 
+UserSchema.methods.generateJWT = function() {
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 60);
+  return (this.token = jwt.sign(
+    {
+      email: this.email,
+      id: this._id,
+      exp: parseInt((expirationDate.getTime() / 100).toString(), 10)
+    },
+    "secret"
+  ));
+};
+
+UserSchema.methods.generateJWT = function() {
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 60);
+  return (this.token = jwt.sign(
+    {
+      email: this.email,
+      id: this._id,
+      exp: parseInt((expirationDate.getTime() / 100).toString(), 10)
+    },
+    "secret"
+  ));
+};
+
+UserSchema.methods.toAuthJSON = function() {
+  return {
+    _id: this._id,
+    email: this.email,
+    salt: this.salt,
+    token: this.token,
+    password: this.setPassword(this.password)
+  };
+};
 UserSchema.method({});
 
 /**
@@ -39,4 +86,4 @@ UserSchema.static({});
  * Register
  */
 
-mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
