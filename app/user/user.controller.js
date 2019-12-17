6aed7ed767acee7c4dac15b1c3327e7bd17e9f6e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-18 00:40:42 +0100

    Create the logic for registering a customer

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -4,6 +4,30 @@ exports.registerUser = async (req, res) => {
   try {
     const value = req.body;
     const data = await UserService.registerUser(value);
+    console.log(data);
+    if (data.error) {
+      return res.status(400).json({
+        success: false,
+        message: data.msg
+      });
+    }
+
+    return res.status(201).json({
+      success: true,
+      message: data
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+exports.authenticate = async (req, res) => {
+  try {
+    const value = req.body;
+    const data = await UserService.registerUser(value);
 
     if (data.error) {
       return res.status(400).json({
