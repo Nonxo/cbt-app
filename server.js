6cf117e0a7dbe6a57fb9bff4a58b3797fd4ec929
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-18 00:40:14 +0100

    Commented unwanted boiler plate code

--- server.js
+++ server.js
@@ -12,15 +12,15 @@
 
 require("dotenv").config();
 
-const fs = require("fs");
-const join = require("path").join;
+// const fs = require("fs");
+// const join = require("path").join;
 const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const config = require("./config/env/development");
 const router = require("./config/routes");
 
-const models = join(__dirname, "app/models");
+// const models = join(__dirname, "app/models");
 // const port = process.env.PORT || 3000;
 
 const app = express();
@@ -35,10 +35,10 @@ module.exports = {
   connection
 };
 
-// Bootstrap models
-fs.readdirSync(models)
-  .filter(file => ~file.indexOf(".js"))
-  .forEach(file => require(join(models, file)));
+// // Bootstrap models
+// fs.readdirSync(models)
+//   .filter(file => ~file.indexOf(".js"))
+//   .forEach(file => require(join(models, file)));
 
 // Bootstrap routes
 require("./config/passport")(passport);
