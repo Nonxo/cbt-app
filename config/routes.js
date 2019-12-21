e7c828816ab70c7535d0097037cbe0b0faecb9af
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-21 14:44:04 +0100

    Remove zombie code

--- config/routes.js
+++ config/routes.js
@@ -11,37 +11,3 @@ const router = express.Router();
 
 router.use("/users", user);
 module.exports = router;
-/**
- * Expose
- */
-
-// module.exports = function(app) {
-//   app.get("/", home.index);
-//   app.post("/register", user);
-
-//   /**
-//    * Error handling
-//    */
-
-//   app.use(function(err, req, res, next) {
-//     // treat as 404
-//     if (
-//       err.message &&
-//       (~err.message.indexOf("not found") ||
-//         ~err.message.indexOf("Cast to ObjectId failed"))
-//     ) {
-//       return next();
-//     }
-//     console.error(err.stack);
-//     // error page
-//     res.status(500).render("500", { error: err.stack });
-//   });
-
-//   // assume 404 since no middleware responded
-//   app.use(function(req, res) {
-//     res.status(404).render("404", {
-//       url: req.originalUrl,
-//       error: "Not found"
-//     });
-//   });
-// };
