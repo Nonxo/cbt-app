e25933183067ed62991e7a44b882bd4e16ee3e9c
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-26 23:05:22 +0100

    Added path,smtpTransport  and reset password configuration

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -1,4 +1,5 @@
 const UserService = require("./user.service");
+const path = require("path");
 
 exports.registerUser = async (req, res) => {
   try {
@@ -46,4 +47,38 @@ exports.authenticate = async (req, res) => {
       message: error.message
     });
   }
+};
+
+exports.forgotPassword = async (req, res) => {
+  try {
+    const value = req.body;
+    const data = await UserService.forgotPassword(value);
+    if (data.error) {
+      return res.status(422).json({
+        success: true,
+        message: data.message
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: data
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+exports.index = function(req, res) {
+  return res.sendFile("../views/home/index.html");
+};
+
+exports.renderForgotPasswordTemplate = function(req, res) {
+  return res.sendFile(path.resolve("../views/home/forgot-password.html"));
+};
+
+exports.renderResetPasswordTemplate = function(req, res) {
+  return res.sendFile(path.resolve("../views/home/reset-password.html"));
 };
--- app/user/user.service.js
+++ app/user/user.service.js
@@ -1,5 +1,23 @@
 const User = require("./user.model");
+const email = process.env.MAILER_EMAIL_ID;
+const pass = process.env.MAILER_PASS;
+const nodemailer = require("nodemailer");
+const handleBars = require("nodemailer-express-handlebars");
+const path = require("path");
 
+const smtpTransport = nodemailer.createTransport({
+  service: process.env.MAILER_SERVICE_PROVIDER || "Gmail",
+  auth: email,
+  pass: pass
+});
+
+const handleBarsOption = {
+  viewEngine: "handlebars",
+  viewPath: path.resolve("../views/"),
+  extName: ".html"
+};
+
+smtpTransport.use("compile", handleBars(handleBarsOption));
 exports.registerUser = async function(data) {
   try {
     const newUser = new User({
@@ -65,6 +83,94 @@ exports.authenticate = async function(data) {
       error: false,
       UserDetails
     };
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+exports.forgotPassword = async function(data) {
+  try {
+    const registeredUser = new User({
+      email: data.email
+    });
+    const validEmail = await User.findOne({
+      email: data.email
+    });
+    const { _id, email: userEmail, firstName, lastName } = validEmail;
+    if (!validEmail) {
+      return {
+        error: true,
+        msg: "User not found"
+      };
+    }
+    User.findOneAndUpdate({ _id }, { token });
+    const token = registeredUser.generateJWT();
+
+    let forgotPasswordDetails = {
+      to: userEmail,
+      from: email,
+      template: "Forgot Password Email",
+      subject: "Password reset has arrived",
+      context: {
+        url: "http://localhost:3000/auth/reset_password?token=" + token,
+        name: `${firstName} ${lastName}`
+      }
+    };
+    smtpTransport.sendMail(forgotPasswordDetails, function(err) {
+      if (!err) {
+        return {
+          error: false,
+          msg: "Kindly check your email for further instructions "
+        };
+      }
+      return {
+        error: true,
+        msg: err.message
+      };
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+exports.resetPassword = async function(data) {
+  try {
+    const validUser = await User.findOne({
+      token: data.token
+    });
+    if (validUser) {
+      if (data.newPassword === data.verifyPassword) {
+        validUser.setPassword(data.password);
+      } else
+        return {
+          error: true,
+          msg: "Password does not match"
+        };
+    }
+    const user = validUser.save();
+    const { firstName, email: userEmail, lastName } = user;
+    const resetData = {
+      to: userEmail,
+      from: email,
+      template: "Reset Password email",
+      subject: "Password Reset Confirmation",
+      context: {
+        name: `${firstName} ${lastName}`
+      }
+    };
+
+    smtpTransport.sendMail(resetData, function(err) {
+      if (!err) {
+        return {
+          error: false,
+          msg: "Password Reset"
+        };
+      } else
+        return {
+          error: true,
+          msg: "Password does not match"
+        };
+    });
   } catch (error) {
     throw new Error(error);
   }
