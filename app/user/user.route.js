0419c6d1cd0172532ad16b5cfcddefb4838c5001
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-31 10:48:21 +0100

    Created content for loading the url

--- app/user/user.route.js
+++ app/user/user.route.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const UserController = require("./user.controller");
 
 const { registerUser, authenticate } = UserController;
+router.get("/", function(req, res) {
+  res.status(200).sendFile(__dirname + "/app/views/home/index.html");
+});
 router.post("/", registerUser);
 router.post("/login", authenticate);
 
