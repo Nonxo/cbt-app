5af8c7628a9c764570697644ea95b27def20e7fe
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-31 10:13:57 +0100

    Modified express.js and mongoose connection

--- config/env/production.js
+++ config/env/production.js
@@ -4,7 +4,8 @@
 const dotenv = require("dotenv");
 dotenv.config();
 module.exports = {
-  MONGODB_URL:
-    process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/my_app_production",
-  MONGODB: process.env.MONGODB
+  MONGODB_URL: process.env.MONGODB_URL
+  // MONGODB: process.env.MONGODB
 };
+
+//|| "mongodb://127.0.0.1:27017/my_app_production"
