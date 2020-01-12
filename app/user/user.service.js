394bb46ec2a91a9b9b0eecc3d3d26d575fb0c631
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-01-12 21:26:25 +0100

    Create method for forgotPassword, creating a setup for nodemailer using createTransport and sendMail method to send mails

--- app/user/user.service.js
+++ app/user/user.service.js
@@ -1,23 +1,31 @@
 const User = require("./user.model");
+const nodemailer = require("nodemailer");
+// const handleBars = require("nodemailer-express-handlebars");
+// const path = require("path");
+
+require("dotenv").config();
 const email = process.env.MAILER_EMAIL_ID;
 const pass = process.env.MAILER_PASS;
-const nodemailer = require("nodemailer");
-const handleBars = require("nodemailer-express-handlebars");
-const path = require("path");
-
 const smtpTransport = nodemailer.createTransport({
-  service: process.env.MAILER_SERVICE_PROVIDER || "Gmail",
-  auth: email,
-  pass: pass
+  service: "Gmail",
+  auth: {
+    user: email,
+    pass
+  }
 });
 
-const handleBarsOption = {
-  viewEngine: "handlebars",
-  viewPath: path.resolve("../views/"),
-  extName: ".html"
-};
+// const handleBarsOption = {
+//   viewEngine: {
+//     extName: ".hbs",
+//     partialsDir: path.resolve(__dirname + "app/views"),
+//     layoutsDir: path.resolve(__dirname + "app/views"),
+//     defaultLayout: path.resolve(__dirname + "app/views")
+//   },
+//   viewPath: path.resolve(__dirname + "app/views"),
+//   extName: ".html"
+// };
 
-smtpTransport.use("compile", handleBars(handleBarsOption));
+// smtpTransport.use("compile", handleBars(handleBarsOption));
 exports.registerUser = async function(data) {
   try {
     const newUser = new User({
@@ -91,46 +99,88 @@ exports.authenticate = async function(data) {
 exports.forgotPassword = async function(data) {
   try {
     const registeredUser = new User({
-      email: data.email
+      email: data
     });
+    // console.log(registeredUser);
+    // console.log(data);
     const validEmail = await User.findOne({
-      email: data.email
+      email: data
     });
-    const { _id, email: userEmail, firstName, lastName } = validEmail;
+
+    // console.log("We are valid also" + validEmail);
     if (!validEmail) {
       return {
         error: true,
         msg: "User not found"
       };
     }
-    User.findOneAndUpdate({ _id }, { token });
+    const { _id, email: userEmail } = validEmail;
     const token = registeredUser.generateJWT();
+
+    await User.findOneAndUpdate({ _id }, { token });
+    let smtpTransport = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: email,
+        pass
+      }
+    });
 
     let forgotPasswordDetails = {
       to: userEmail,
       from: email,
-      template: "Forgot Password Email",
-      subject: "Password reset has arrived",
-      context: {
-        url: "http://localhost:3000/auth/reset_password?token=" + token,
-        name: `${firstName} ${lastName}`
-      }
+      text:
+        "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
+        "Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n" +
+        `http://localhost:3000/auth/reset_password?token=${token}\n\n` +
+        "If you did not request this, please ignore this email and your password will remain unchanged.\n",
+      subject: "Link to Reset Password"
+      // context: {
+      //   url: "http://localhost:3000/auth/reset_password?token=" + token,
+      //   name: `${firstName} ${lastName}`
+      // }
     };
-    smtpTransport.sendMail(forgotPasswordDetails, function(err) {
-      if (!err) {
+    let info = await smtpTransport.sendMail(forgotPasswordDetails, function(
+      err,
+      response
+    ) {
+      if (err) {
+        console.log(err);
+        return {
+          error: true,
+          msg: err
+        };
+      } else {
+        console.log("Here is the response", response);
         return {
           error: false,
-          msg: "Kindly check your email for further instructions "
+          msg: "Kindly check your email for further instructions ",
+          response
         };
       }
-      return {
-        error: true,
-        msg: err.message
-      };
     });
+    console.log("My info", info);
+    return {
+      error: false,
+      message: "Success"
+    };
   } catch (error) {
     throw new Error(error);
   }
+};
+
+exports.validPasswordToken = async function(data) {
+  try {
+    let validToken = await User.findOne({
+      where: {
+        resetPasswordToken: data
+      }
+    });
+
+    if (validToken == null) {
+      cons
+    }
+  } catch (error) {}
 };
 
 exports.resetPassword = async function(data) {
