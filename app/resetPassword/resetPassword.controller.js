b5ee6bef9324fe29adcd8928eb54c5d5cb21a4b4
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-02-17 21:42:06 +0100

    create controller for forgotPassword method

--- app/resetPassword/resetPassword.controller.js
+++ app/resetPassword/resetPassword.controller.js
@@ -1,18 +1,65 @@
-const ResetPasswordService = require("./resetPassword.service");
+const {
+  forgotPassword,
+  // resetPassword,
+  validateToken
+} = require("./resetPassword.service");
+const url = require("url");
 
 exports.forgotPassword = async (req, res) => {
   try {
-    const { email } = req.body;
-    if (email === "") {
+    const value = req.body.email;
+
+    if (!req.body.email) {
       res.status(400).json({
         success: false,
-        message: "email is required"
+        message: "Email is required"
       });
     }
-    const data = await ResetPasswordService.forgotPassword(email);
+    const data = await forgotPassword(value);
+    console.log(data);
     if (data.error) {
-      res.status(200).json({
+      console.log(data.error);
+      return res.status(409).json({
         success: false,
+        message: data.error
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: data
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err
+    });
+  }
+};
+
+exports.validateToken = async (req, res, next) => {
+  try {
+    const queryObject = url.parse(req.url, true).query;
+    if (!queryObject) {
+      res.status(400).json({
+        success: false,
+        message: "Token is required"
+      });
+    }
+    let data = await validateToken(queryObject);
+    // eslint-disable-next-line require-atomic-updates
+    res.locals.data = req.body;
+    if (data.error) {
+      console.log(data.error);
+      return res.status(409).json({
+        success: false,
+        message: data.error
+      });
+    }
+    // console.log(res.locals, "We are here o");
+    next();
+  } catch (err) {
+    res.status(500).json({
+      success: false,
         message: data.msg
       });
     }
