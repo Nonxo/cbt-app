ba56d2a43c85794fc309ef69b89c4850049d714e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-02-17 21:48:19 +0100

    Added the resetPassword base url

--- config/routes.js
+++ config/routes.js
@@ -6,8 +6,10 @@
 
 // const home = require("../app/routes/home");
 const user = require("../app/user/user.route");
+const resetPassword = require("../app/resetPassword/resetPassword.route");
 const express = require("express");
 const router = express.Router();
 
 router.use("/users", user);
+router.use("/", resetPassword);
 module.exports = router;
