d4fc1b97e089d500359f24e905ffe3ab4f8a2ecb
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-02-18 01:07:59 +0100

    Refactored resetPassword controller

--- app/resetPassword/resetPassword.controller.js
+++ app/resetPassword/resetPassword.controller.js
@@ -1,6 +1,6 @@
 const {
   forgotPassword,
-  // resetPassword,
+  resetPassword,
   validateToken
 } = require("./resetPassword.service");
 const url = require("url");
@@ -81,27 +81,8 @@ exports.resetPassword = async (req, res) => {
     }
     return res.status(200).json({
       success: true,
-      message: `It is done`
+      message: result
     });
-    // if (!req.body.email) {
-    //   res.status(400).json({
-    //     success: false,
-    //     message: "Email is required"
-    //   });
-    // }
-    // const data = await forgotPassword(value);
-    // console.log(data);
-    // if (data.error) {
-    //   console.log(data.error);
-    //   return res.status(409).json({
-    //     success: false,
-    //     message: data.error
-    //   });
-    // }
-    // return res.status(200).json({
-    //   success: true,
-    //   message: data
-    // });
   } catch (err) {
     res.status(500).json({
       success: false,
